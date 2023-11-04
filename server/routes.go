ac988ea0329c741746908cab05a371d7fb7eb995
Thanh Nguyen <vnscriptkid@gmail.com>
2023-11-04 17:03:15 +0800

    feat: signup newsletter api

--- server/routes.go
+++ server/routes.go
@@ -1,9 +1,21 @@
 package server
 
-import "canvas/handlers"
+import (
+	"canvas/handlers"
+	"canvas/model"
+	"context"
+)
 
 func (s *Server) setupRoutes() {
 	handlers.Health(s.mux)
 
 	handlers.FrontPage(s.mux)
+	handlers.NewsletterSignup(s.mux, &signupperMock{})
+	handlers.NewsletterThanks(s.mux)
+}
+
+type signupperMock struct{}
+
+func (s signupperMock) SignupForNewsletter(ctx context.Context, email model.Email) (string, error) {
+	return "", nil
 }
