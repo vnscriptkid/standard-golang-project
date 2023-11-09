2dd3a8741abf14ef12342cff49854263638fed41
Thanh Nguyen <vnscriptkid@gmail.com>
2023-11-09 23:52:41 +0700

    feat: alert manager

--- handlers/views.go
+++ handlers/views.go
@@ -10,6 +10,9 @@ import (
 
 func FrontPage(mux chi.Router) {
 	mux.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		// return 500
+		http.Error(w, "error", http.StatusInternalServerError)
+
 		_ = views.FrontPage().Render(w)
 	})
 }
